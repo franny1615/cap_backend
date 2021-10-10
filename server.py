82ea486fe4338869c4d07be335d4c746218c9f71
Francisco <franke1615@gmail.com>
2021-10-10 15:41:26 -0700

    template

---
+++ server.py
@@ -0,0 +1,182 @@
+# source /Users/tnappy/node_projects/quickstart/python/bin/activate
+# Read env vars from .env file
+from plaid.exceptions import ApiException
+from plaid.model.products import Products
+from plaid.model.country_code import CountryCode
+from plaid.model.numbers_bacs_nullable import NumbersBACSNullable
+from plaid.model.item_public_token_exchange_request import ItemPublicTokenExchangeRequest
+from plaid.model.link_token_create_request import LinkTokenCreateRequest
+from plaid.model.link_token_create_request_user import LinkTokenCreateRequestUser
+from plaid.model.auth_get_request import AuthGetRequest
+from plaid.model.transactions_get_request import TransactionsGetRequest
+from plaid.model.transactions_get_request_options import TransactionsGetRequestOptions
+from plaid.model.ach_class import ACHClass
+from plaid.api import plaid_api
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import jsonify
+from datetime import datetime
+from datetime import timedelta
+import plaid
+import base64
+import os
+import datetime
+import json
+import time
+from dotenv import load_dotenv
+from werkzeug.wrappers import response
+load_dotenv()
+
+app = Flask(__name__)
+
+# Fill in your Plaid API keys - https://dashboard.plaid.com/account/keys
+PLAID_CLIENT_ID = os.getenv('PLAID_CLIENT_ID')
+PLAID_SECRET = os.getenv('PLAID_SECRET')
+# Use 'sandbox' to test with Plaid's Sandbox environment (username: user_good,
+# password: pass_good)
+# Use `development` to test with live users and credentials and `production`
+# to go live
+PLAID_ENV = os.getenv('PLAID_ENV', 'sandbox')
+# PLAID_PRODUCTS is a comma-separated list of products to use when initializing
+# Link. Note that this list must contain 'assets' in order for the app to be
+# able to create and retrieve asset reports.
+PLAID_PRODUCTS = os.getenv('PLAID_PRODUCTS', 'transactions').split(',')
+
+# PLAID_COUNTRY_CODES is a comma-separated list of countries for which users
+# will be able to select institutions from.
+PLAID_COUNTRY_CODES = os.getenv('PLAID_COUNTRY_CODES', 'US').split(',')
+
+
+def empty_to_none(field):
+    value = os.getenv(field)
+    if value is None or len(value) == 0:
+        return None
+    return value
+
+host = plaid.Environment.Sandbox
+
+if PLAID_ENV == 'sandbox':
+    host = plaid.Environment.Sandbox
+
+if PLAID_ENV == 'development':
+    host = plaid.Environment.Development
+
+if PLAID_ENV == 'production':
+    host = plaid.Environment.Production
+
+# Parameters used for the OAuth redirect Link flow.
+#
+# Set PLAID_REDIRECT_URI to 'http://localhost:3000/'
+# The OAuth redirect flow requires an endpoint on the developer's website
+# that the bank website should redirect to. You will need to configure
+# this redirect URI for your client ID through the Plaid developer dashboard
+# at https://dashboard.plaid.com/team/api.
+PLAID_REDIRECT_URI = empty_to_none('PLAID_REDIRECT_URI')
+
+configuration = plaid.Configuration(
+    host=host,
+    api_key={
+        'clientId': PLAID_CLIENT_ID,
+        'secret': PLAID_SECRET,
+        'plaidVersion': '2020-09-14'
+    }
+)
+
+api_client = plaid.ApiClient(configuration)
+client = plaid_api.PlaidApi(api_client)
+
+products = []
+for product in PLAID_PRODUCTS:
+    products.append(Products(product))
+
+# We store the access_token in memory - in production, store it in a secure
+# persistent data store.
+access_token = None
+
+item_id = None
+
+@app.route('/api/info', methods=['POST'])
+def info():
+    global access_token
+    global item_id
+    return jsonify({
+        'item_id': item_id,
+        'access_token': access_token,
+        'products': PLAID_PRODUCTS
+    })
+
+
+@app.route('/api/create_link_token', methods=['POST'])
+def create_link_token():
+    try:
+        request = LinkTokenCreateRequest(
+            products=products,
+            client_name="Plaid Quickstart",
+            country_codes=list(map(lambda x: CountryCode(x), PLAID_COUNTRY_CODES)),
+            language='en',
+            user=LinkTokenCreateRequestUser(
+                client_user_id=str(time.time())
+            )
+        )
+
+        # create link token
+        response = client.link_token_create(request)
+        return jsonify(response.to_dict())
+    except plaid.ApiException as e:
+        return json.loads(e.body)
+
+
+# Exchange token flow - exchange a Link public_token for
+# an API access_token
+# https://plaid.com/docs/#exchange-token-flow
+@app.route('/api/set_access_token', methods=['POST'])
+def get_access_token():
+    global access_token
+    global item_id
+    global transfer_id
+    public_token = request.form['public_token']
+    try:
+        exchange_request = ItemPublicTokenExchangeRequest(
+            public_token=public_token)
+        exchange_response = client.item_public_token_exchange(exchange_request)
+        access_token = exchange_response['access_token']
+        item_id = exchange_response['item_id']
+        if 'transfer' in PLAID_PRODUCTS:
+            transfer_id = authorize_and_create_transfer(access_token)
+        return jsonify(exchange_response.to_dict())
+    except plaid.ApiException as e:
+        return json.loads(e.body)
+
+# Retrieve Transactions for an Item
+# https://plaid.com/docs/#transactions
+@app.route('/api/transactions', methods=['GET'])
+def get_transactions():
+    # Pull transactions for the last 30 days
+    start_date = (datetime.datetime.now() - timedelta(days=30))
+    end_date = datetime.datetime.now()
+    try:
+        options = TransactionsGetRequestOptions()
+        request = TransactionsGetRequest(
+            access_token=access_token,
+            start_date=start_date.date(),
+            end_date=end_date.date(),
+            options=options
+        )
+        response = client.transactions_get(request)
+        pretty_print_response(response.to_dict())
+        return jsonify(response.to_dict())
+    except plaid.ApiException as e:
+        error_response = format_error(e)
+        return jsonify(error_response)
+
+def pretty_print_response(response):
+  print(json.dumps(response, indent=2, sort_keys=True, default=str))
+
+def format_error(e):
+    response = json.loads(e.body)
+    return {'error': {'status_code': e.status, 'display_message':
+                      response['error_message'], 'error_code': response['error_code'], 'error_type': response['error_type']}}
+
+if __name__ == '__main__':
+    app.run(port=os.getenv('PORT', 8000))
